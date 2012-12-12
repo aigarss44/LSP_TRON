2c67ba1fc0c1ea323ed81ce40515b38eaf75a71f
rhcake <rikus2@inbox.lv>
2012-12-12 21:15:14 +0200

    glut window

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
   glutInitWindowPosition(50,25);
   glutInitWindowSize(500,250);
-  glutCreateWindow("Soft gavno");
+  glutCreateWindow("window");
   glutDisplayFunc(draw);
   glutMainLoop();
   return 0;
