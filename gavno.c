dc7b5962a7b043df45e3342ba726e020e0948896
rhcake <rikus2@inbox.lv>
2012-12-12 21:09:03 +0200

    Simple glut window

---
+++ gavno.c
@@ -0,0 +1,20 @@
+#include <GL/glut.h>
+
+void draw(void)
+{
+  glClearColor(0,0,1,1);
+  glClear(GL_COLOR_BUFFER_BIT );
+  glFlush();
+}
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
+  glutInitWindowPosition(50,25);
+  glutInitWindowSize(500,250);
+  glutCreateWindow("Soft gavno");
+  glutDisplayFunc(draw);
+  glutMainLoop();
+  return 0;
+}
