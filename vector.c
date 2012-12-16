5ed49549a7182b6b08067e927960ba2594c3b4c0
aigarss44 <aigars.silavs@gmail.com>
2012-12-16 14:35:32 +0200

    Nestrādājošas vektoru/ matemātikas metodes.
    Iekš vector.c liksim arī rotatorus un matricu metodes ja tādas vajadzēs.
    Visu general matemātiku kuru var vajadzet vairāk kā vienā vietā.

---
+++ vector.c
@@ -0,0 +1,41 @@
+#include "vector.h"
+
+//vector gavno
+/*
+struct Vect3d {
+
+  float x, y, z; 
+
+  Vect3d(float X=0, float Y=0, float Z=0)
+  { x=X; y=Y; z=Z; }
+
+  Vect3d operator + (const Vect3d &b) const 
+  { return Vect3d(x+b.x,y+b.y,z+b.z); }
+
+  Vect3d operator - (const Vect3d &b) const 
+  { return Vect3d(x-b.x,y-b.y,z-b.z); }
+
+  Vect3d operator * (float b) const 
+  { return Vect3d(x*b,y*b,z*b); }
+
+  Vect3d mult (const Vect3d &b) const 
+  { return Vect3d(x*b.x,y*b.y,z*b.z); }
+
+};
+
+struct Vect2d {
+
+  float x, y; 
+
+  Vect2d (float X=0, float Y=0) { x=X; y=Y; }
+
+  Vect2d operator + (const Vect2d &b) const { return Vect2d(x+b.x,y+b.y); }
+
+  Vect2d operator - (const Vect2d &b) const { return Vect2d(x-b.x,y-b.y); }
+
+  Vect2d operator * (float b) const { return Vect2d(x*b,y*b); }
+
+  Vect2d mult (const Vect2d &b) const { return Vect2d(x*b.x,y*b.y); }
+
+};
+*/
---
+++ vector.h
@@ -0,0 +1,4 @@
+#include <math.h>
+
+struct Vect2d;
+struct Vect2d;
