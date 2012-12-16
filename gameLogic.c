0a3515ee61899a698fdf3453768d4faad2da3ff0
rhcake <rikus2@inbox.lv>
2012-12-16 14:28:36 +0200

    compile fix*

--- gameLogic.c
+++ gameLogic.c
@@ -1,29 +1,38 @@
-#include "gameLogic.h";
+#include "gameLogic.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 
 void AddNewPlayer(float x, float y, float angle, Field *field) {
+	
 	Lightcycle *player = (Lightcycle *) malloc(sizeof(Lightcycle));
+	Lightcycle *p;
 	//setojam sâkotnçjâs koordinâtas
 	player->angle = angle;
 	player->x = x;
 	player->y = y;
 	player->next = NULL;
 	player->tailLenght = 0;
-
+	
 	//soft gavno
-	Lightcycle *p = field->player;
+	p = field->player;
 	while (p != NULL) {
 		p = p->next;
 	}
 	p->next = player;
+	
 }
 
 void StartGame () {
+
+	Lightcycle *me;
+
 	Field *field = (Field *) malloc(sizeof(Field));
 	field->height = FIELD_HEIGHT;
-	field->width - FIELD_HEIGHT;
-	Lightcycle *me = (Lightcycle *) malloc(sizeof(Lightcycle));
-	//field->player = me; kas viòam nepatîk?
+	field->width = FIELD_WIDTH;
+
+	me = (Lightcycle *) malloc(sizeof(Lightcycle));
+	field->player = me;
 
 }
 
--- gameLogic.h
+++ gameLogic.h
@@ -1,30 +1,29 @@
-#ifndef STDIO_H
-#define STDIO_H
 #include <stdio.h>
 #include <stdlib.h>
-#endif // !STDIO_H
-#include "vector.h"
+//#include "vector.h"
 
 #define FIELD_WIDTH 160		//lauka pa kuru brauc motocikli izmçri
 #define FIELD_HEIGHT 90
 #define MAX_TAIL_LENGTH 100
 
-typedef struct Field ;
-typedef struct Lightcycle ;
 
-typedef struct {
-	float width;
-	float height;
-	struct Lightcycle *player;
-} Field;
-
-typedef struct {
+typedef struct Lightcycle Lightcycle;
+struct Lightcycle {
 	float x;
 	float y;
 	float angle;	//leòíis grâdos
 	int tailLenght;
-	struct Lightcycle *next;
+	Lightcycle *next;
 	float tail[MAX_TAIL_LENGTH]; //bûs 2d vektori nevis floati
-} Lightcycle;
+};
+
+typedef struct Field Field;
+struct Field {
+	float width;
+	float height;
+	Lightcycle *player;
+};
+
+
 
 void gameTick();
